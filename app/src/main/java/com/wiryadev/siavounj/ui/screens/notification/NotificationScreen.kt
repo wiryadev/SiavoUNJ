1603f13095cae8c7a70a926573988d1016b47e5c
wiryadev <abrarwiryawan@gmail.com>
2022-11-29 19:11:00 +0700

    create notification preview screen

--- app/src/main/java/com/wiryadev/siavounj/ui/screens/notification/NotificationScreen.kt
+++ app/src/main/java/com/wiryadev/siavounj/ui/screens/notification/NotificationScreen.kt
@@ -1,12 +1,25 @@
 package com.wiryadev.siavounj.ui.screens.notification
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -17,13 +30,37 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.wiryadev.siavounj.R
-import com.wiryadev.siavounj.ui.theme.*
+import com.wiryadev.siavounj.data.model.previewNotificationPreview
+import com.wiryadev.siavounj.ui.components.NotificationPreviewItem
+import com.wiryadev.siavounj.ui.theme.Debutante500
+import com.wiryadev.siavounj.ui.theme.Debutante600
+import com.wiryadev.siavounj.ui.theme.SiavoUNJTheme
+import com.wiryadev.siavounj.ui.theme.body1SemiBold
+import com.wiryadev.siavounj.ui.theme.body2Bold
+import com.wiryadev.siavounj.ui.theme.body3
+import com.wiryadev.siavounj.ui.theme.body3Medium
 
 @Composable
 fun NotificationScreen(
     modifier: Modifier = Modifier,
 ) {
-
+    Column(
+        modifier = modifier,
+    ) {
+        NotificationAppBar(
+            notificationsAmount = previewNotificationPreview.size,
+        )
+        LazyColumn {
+            items(previewNotificationPreview) { item ->
+                NotificationPreviewItem(
+                    title = item.title,
+                    dateTime = item.dateTime,
+                    isRead = item.isRead,
+                )
+                Divider()
+            }
+        }
+    }
 }
 
 @Composable
@@ -31,8 +68,17 @@ fun NotificationAppBar(
     notificationsAmount: Int,
     modifier: Modifier = Modifier,
 ) {
+    val badgeHorizontalPadding by remember {
+        derivedStateOf {
+            if (notificationsAmount > 9) 8 else 12
+        }
+    }
+
     Column(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .padding(bottom = 16.dp),
     ) {
         Text(
             text = "SIAVO UNJ",
@@ -41,7 +87,10 @@ fun NotificationAppBar(
             textAlign = TextAlign.Center,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 16.dp),
+                .padding(
+                    top = 24.dp,
+                    bottom = 16.dp,
+                ),
         )
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
@@ -49,7 +98,7 @@ fun NotificationAppBar(
         ) {
             Row {
                 Text(
-                    text = "SIAVO UNJ",
+                    text = stringResource(R.string.my_notifications),
                     style = body2Bold,
                     color = Debutante600,
                 )
@@ -60,7 +109,7 @@ fun NotificationAppBar(
                         .background(Debutante500)
                         .padding(
                             vertical = 2.dp,
-                            horizontal = 8.dp,
+                            horizontal = badgeHorizontalPadding.dp,
                         ),
                 ) {
                     Text(
